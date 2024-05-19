4e4a6f9325b79f3b9799ddeec5204fda6a094388
Luis-Berrio <laberrio@unicesar.edu.co>
2024-05-19 02:11:45 -0500

    add emailjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,44 +136,6 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("[data-form]");
-  const response = document.getElementById("response");
-  const submitButton = document.querySelector("[data-form-btn]");
-
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    // Obtener los datos del formulario
-    const fullname = form.querySelector('input[name="fullname"]').value;
-    const email = form.querySelector('input[name="email"]').value;
-    const message = form.querySelector('textarea[name="message"]').value;
-
-    // Parámetros para la plantilla de EmailJS
-    const templateParams = {
-      from_name: fullname,
-      from_email: email,
-      message: message,
-    };
-
-    // Enviar el correo electrónico usando EmailJS
-    emailjs.send("yaYC-dUuSO2SSkG5E", "template_m77z8d9", templateParams).then(
-      function (response) {
-        console.log("SUCCESS!", response.status, response.text);
-        response.innerHTML = "¡Mensaje enviado exitosamente!";
-        form.reset();
-      },
-      function (error) {
-        console.error("FAILED...", error);
-        response.innerHTML =
-          "Hubo un error al enviar el mensaje. Por favor, inténtalo de nuevo.";
-      }
-    );
-  });
-
-  // Habilitar el botón de envío cuando todos los campos estén completos
-});
-
 const btn = document.getElementById("form-btn");
 
 document.getElementById("form").addEventListener("submit", function (event) {
