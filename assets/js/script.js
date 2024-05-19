cc516aa97e16d399229de85d6013036a479ba431
Luis-Berrio <laberrio@unicesar.edu.co>
2024-05-19 02:52:50 -0500

    add email new

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,26 +136,24 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-const btn = document.getElementById("btn");
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.getElementById('btn');
 
-document
-  .getElementById("form-btn")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+  document.getElementById('form-email').addEventListener('submit', function(event) {
+      event.preventDefault();
 
-    btn.value = "Sending...";
+      btn.value = 'Sending...';
 
-    const serviceID = "default_service";
-    const templateID = "template_m77z8d9";
+      const serviceID = 'default_service';
+      const templateID = 'template_m77z8d9';
 
-    emailjs.sendForm(serviceID, templateID, this).then(
-      () => {
-        btn.value = "Send Email";
-        alert("Sent!");
-      },
-      (err) => {
-        btn.value = "Send Email";
-        alert(JSON.stringify(err));
-      }
-    );
+      emailjs.sendForm(serviceID, templateID, this)
+          .then(() => {
+              btn.value = 'Send Email';
+              alert('Sent!');
+          }, (err) => {
+              btn.value = 'Send Email';
+              alert(JSON.stringify(err));
+          });
   });
+});
